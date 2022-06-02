3084c4a5079d49a672222ed8f3a0f84ce58d43a3
Paavo <paavo.hietala@aalto.fi>
2022-06-02 16:55:54 +0300

    Manifest v2->v3 and refactoring

--- popup.js
+++ popup.js
@@ -3,24 +3,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     var libpButton = document.getElementById('fixUrl');
     libpButton.addEventListener('click', function() {
-        chrome.tabs.getSelected(null, function(tab) {
-            // Get current tab URL and ID
-            chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>{
-                let url = tabs[0].url;
-                let id = tabs[0].id; 
-                processUrl(url, id);
-            });
+		// Get current tab URL and ID
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>{
+			let url = tabs[0].url;
+			let id = tabs[0].id;
+			
+			processUrl(url, id);
+		});
 
-            // Process the URL and go to the new address
-            function processUrl(url, id){
-                url = url.replace(/^https?:\/\//,''); // remove http or https
-                url = url.split(/\/(.+)/);            // split by the first slash to get base url
-                end = url[1];
-                base = url[0].split(".");
-                final = "https://" + base.join("-") + ".libproxy.aalto.fi" + "/" + end;
-                console.log(final)
-                chrome.tabs.update(id, {url: final});
-            };
-      });
+		// Process the URL and go to the new address
+		function processUrl(url, id){
+			url = url.replace(/^https?:\/\//,''); // remove http or https
+			url = url.split(/\/(.+)/);            // split by the first slash to get base url
+			final = "https://" + url[0].split(".").join("-") + ".libproxy.aalto.fi" + "/" + url[1];
+			chrome.tabs.update(id, {url: final});
+		};
     }, false);
   }, false);
