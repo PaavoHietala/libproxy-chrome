4135f8af63d0e6c82ef4cbeaad15f2a2785fdd46
Paavo Hietala <paavo.hietala@aalto.fi>
2022-07-25 16:11:43 +0300

    V2.0, major restructuring, changed icons, readme

--- README.md
+++ README.md
@@ -10,9 +10,17 @@ turns into
 
 > https://ieeexplore-ieee-org.libproxy.aalto.fi/document/7298573
 
-## Usage
+## Installation
 
 1. Clone the repository to your computer
 2. Go to `chrome://extensions` and enable `Developer mode`
 3. Click `Load unpacked` and select the `libproxy-chrome` folder
-4. Pin the extension to your toolbar
+4. Pin the extension to your toolbar by clicking the extensions icon in top right corner and clicking the pin next to the "Aalto LibProxy wrapper plugin".
+
+## Usage
+
+1. Navigate to the article you want to read
+2. Click on the blue LP icon in your extensions toolbar
+3. Click "LibProxy this!"
+4. Input your Aalto credentials if you haven't already logged in
+5. In the unlikely case of a 404 or other error you can return with the "Go back..." button
--- popup.js
+++ popup.js
@@ -1,36 +1,46 @@
 // Change the current tab's url to its libproxy equivalent
 
-// Process the URL and go to the new address
+/**
+ * Process the URL according to mode parameter
+ * 
+ * @param {string} url Complete URL sent for processing
+ * @param {number} id Chrome active tab ID
+ * @param {string} mode "unlock" for opening via libproxy, "revert" for going back
+ * @returns {string} Final processed URL 
+ */
 function processUrl(url, id, mode){
-	url = url.replace(/^https?:\/\//,''); // remove http or https
-	url = url.split(/\/(.+)/);            // split by the first slash to get base url
-	if (mode == "unlock") {
-		final = "https://" + url[0].replaceAll(".", "-") + ".libproxy.aalto.fi" + "/" + url[1];
-	} else {
-		final = "https://" + url[0].replace(".libproxy.aalto.fi","").replaceAll("-",".") + "/" + url[1];
-	}
-	chrome.tabs.update(id, {url: final});
-	window.close();
+    url = url.replace(/^https?:\/\//,''); // remove http or https
+    url = url.split(/\/(.+)/);            // split by the first slash to get base url
+    if (mode == "unlock") {
+        final = "https://" + url[0].replaceAll(".", "-") + ".libproxy.aalto.fi" + "/" + url[1];
+    } else {
+        final = "https://" + url[0].replace(".libproxy.aalto.fi","").replaceAll("-",".") + "/" + url[1];
+    }
+
+    return final;
 };
 
+/**
+ * Change the active tab to the URL processed based on the mode parameter
+ * 
+ * @param {string} mode "unlock" for opening via libproxy, "revert" for going back
+ */
+function tabUpdater(mode){
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>{
+        let url = tabs[0].url;
+        let id = tabs[0].id;
+        final = processUrl(url, id, mode);
+
+        chrome.tabs.update(id, {url: final});
+        window.close();
+    });
+}
+
+// Set the tabUpdater listeners for popup buttons
 document.addEventListener('DOMContentLoaded', function() {
-    var libpButton = document.getElementById('fixUrl');
-    libpButton.addEventListener('click', function() {
-		// Get current tab URL and ID
-		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>{
-			let url = tabs[0].url;
-			let id = tabs[0].id;
-			processUrl(url, id, "unlock");
-		});
-    }, false);
-	
-	var revertButton = document.getElementById('revert');
-    revertButton.addEventListener('click', function() {
-		// Get current tab URL and ID
-		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>{
-			let url = tabs[0].url;
-			let id = tabs[0].id;
-			processUrl(url, id, "revert");
-		});
-    }, false);
-  }, false);
+    const libpButton = document.getElementById('fixUrl');
+    libpButton.addEventListener('click', tabUpdater.bind(null, "unlock"));
+    
+    const revertButton = document.getElementById('revert');
+    revertButton.addEventListener('click', tabUpdater.bind(null, "revert"));
+}, false);
